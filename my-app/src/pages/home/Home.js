55f15e15f9114beece1c67de510aac7330111c5e
Ahmed Elshewemy <96390565+AhmedElshewemy@users.noreply.github.com>
2024-10-21 14:39:25 +0300

    Add files via upload

--- my-app/src/pages/home/Home.js
+++ my-app/src/pages/home/Home.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ProductCard from'../../components/ProductCard'
 import { Form } from "react-bootstrap";
+import axios from "axios";
+import Spinner from 'react-bootstrap/Spinner';
+
 const Home =()=>{
+   
+   
+    const [propertys,setProperty]=useState({
+        loading:true,
+        results:[],
+        err:null,
+        reload:0
+    });
+
+    useEffect(()=>{
+        axios.get("https://redsea.runasp.net/api/Property/GetAll")
+        .then(res=>{
+            console.log(res);
+            setProperty({...propertys,results: res.data, loading: false, err:null})
+
+        }).catch(err=>{ setProperty({...propertys,loading: false, err: 'ops something went wrong '})
+        })
+
+    },[])
     return(
+
+   
     <div className="home-container p-5"> 
-    {
-        // filter
-    }
+    {/* loader */}
+    {propertys.loading === true &&(
+   <div className="text-center">
+   <Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
+   </div>
+
+)}
+    
+    {propertys.loading === false && (
+    <>
+    
+    {  /* filter*/ }
       <Form>
       <Form.Group className="mb-3 d-flex">
         <Form.Control type="text" placeholder="Search" className="rounded-0"/>
@@ -34,6 +69,10 @@ const Home =()=>{
             <ProductCard />
             </div>
         </div>
+    
+    </>
+    )}
+    
     
     </div>);
 
